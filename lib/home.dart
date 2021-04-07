8bb1ad22d7011d0c9e0dd21c4367402d8c7eef43
mingabire809 <michelingabire190508@daystar.ac.ke>
2021-04-07 15:00:24 +0300

    second commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:assurance/login.dart';
 import 'package:flutter/material.dart';
 import 'category.dart';
+import 'menu.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -17,36 +18,45 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.black,
-          size: 20.0,
-          semanticLabel: 'Menu',
-
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(
+            icon: const Icon(
+              Icons.menu,
+              color: Colors.black,
+              size: 20.0,
+              semanticLabel: 'Menu',
+            ),
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+          );
+        }),
+        title: Text(
+          "Assurance",
+          style: TextStyle(color: Colors.black),
         ),
-        title: Text("Assurance",
-          style: TextStyle(color: Colors.black),),
         actions: <Widget>[
-          IconButton(icon: Icon(Icons.search),color: Colors.black, iconSize: 28.0, onPressed: null),
           IconButton(
-              icon: Icon(Icons.category),color: Colors.black, iconSize: 28.0,
-
-              onPressed: (){
-               Navigator.push(
-                 context,
-                 MaterialPageRoute(builder: (context) => Category())
-               ) ;
-
-              }
-              ),
+              icon: Icon(Icons.search),
+              color: Colors.black,
+              iconSize: 28.0,
+              onPressed: null),
           IconButton(
-              icon: Icon(Icons.logout),color: Colors.black, iconSize: 28.0,
-        
-              onPressed: (){
+              icon: Icon(Icons.category),
+              color: Colors.black,
+              iconSize: 28.0,
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => Category()));
+              }),
+          IconButton(
+              icon: Icon(Icons.logout),
+              color: Colors.black,
+              iconSize: 28.0,
+              onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Login())
-                ) ;
+                    context, MaterialPageRoute(builder: (context) => Login()));
               }),
         ],
       ),
@@ -63,79 +73,97 @@ class _MyHomePageState extends State<MyHomePage> {
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/socabu.png"),
               ),
-              title: Text("SOCABU",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "SOCABU",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/ascoma.png"),
               ),
-              title: Text("ASCOMA",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "ASCOMA",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/bicor.png"),
               ),
-              title: Text("BICOR",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "BICOR",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/jubilee.jpg"),
               ),
-              title: Text("JUBILEE",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "JUBILEE",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/mfp.jpg"),
               ),
-              title: Text("MUTUELLE DE LA FONCTION PUBLIQUE",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "MUTUELLE DE LA FONCTION PUBLIQUE",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/solis.png"),
               ),
-              title: Text("MUTUALITE SOLIS",
-                style: TextStyle(color: Colors.white),),),
+              title: Text(
+                "MUTUALITE SOLIS",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/egicnv.jpg"),
               ),
-              title: Text("EAST AFRICA GLOBAL INSURANCE COMPANY NON-VIE",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "EAST AFRICA GLOBAL INSURANCE COMPANY NON-VIE",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/socar.jpg"),
               ),
-              title: Text("SOCAR",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "SOCAR",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/ucar.jpg"),
               ),
-              title: Text("UCAR",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "UCAR",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
             ListTile(
               leading: CircleAvatar(
                 backgroundImage: AssetImage("images/bic.jpg"),
               ),
-              title: Text("BUSINESS INSURANCE & REINSURANCE",
-                style: TextStyle(color: Colors.white),),
+              title: Text(
+                "BUSINESS INSURANCE & REINSURANCE",
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-
-
-
-
           ],
         ),
       ),
       // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: Menu(),
     );
   }
 }
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,6 +1,49 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
-Class Menu extends StatelessWidget{
-  const Menu({Key : key})
+
+class Menu extends StatelessWidget {
+  const Menu({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    const drawerHeader = UserAccountsDrawerHeader(
+      accountName: Text("UserName"),
+      accountEmail: Text("user.name@email.com"),
+      decoration: BoxDecoration(color: Colors.black38),
+      currentAccountPicture: CircleAvatar(
+        backgroundImage: AssetImage("images/profile.jpg"),
+      ),
+    );
+
+    final drawerItems = ListView(
+      children: <Widget>[
+        drawerHeader,
+        ListTile(
+          leading: Icon(Icons.home),
+          title: Text("Home"),
+        ),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("Profile"),
+        ),
+        ListTile(
+          leading: Icon(Icons.update),
+          title: Text("Update"),
+        ),
+        ListTile(
+          leading: Icon(Icons.question_answer_rounded),
+          title: Text("About us"),
+        ),
+        ListTile(
+          leading: Icon(Icons.logout),
+          title: Text("Logout"),
+        ),
+      ],
+    );
+    return Scaffold(
+      body: Container(
+        child: drawerItems,
+        color: Colors.white,
+      ),
+    );
+  }
 }
